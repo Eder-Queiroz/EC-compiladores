21d125ae26b9e46c7a8ef40d23bb896f6fc67752
Eder Queiroz <eder.queiroz@graodireto.com.br>
2026-09-09 15:28:40 -0300

    chore: ignore sdd workspace and use fibonacci as pascal sample

--- pascal-lexer/samples/teste.pas
+++ pascal-lexer/samples/teste.pas
@@ -1,53 +1,22 @@
-program exemplo;
-{ Este comentario usa chaves e o analisador deve descarta-lo por inteiro }
-var
-  x, y : integer;
-  total : integer;
-
-function dobro(n : integer) : integer;
-begin
-  dobro := n * 2
-end;
-
-procedure mostrar(a, b : integer);
-begin
-  writeln('valores lidos: ');
-  write(a, b)
-end;
-
-begin
-  read(x, y);
-  total := 0;
-  for x := 1 to 10 do
-  begin
-    total := total + x - 1
-  end;
-  while (total > 100) do
-  begin
-    total := total / 2
-  end;
-  repeat
-    total := total + 1
-  until (total >= 200);
-  (* Este comentario usa parenteses com asterisco e tambem e descartado *)
-  if (x < y) then
-  begin
-    write(x)
-  end;
-  if (x = y) and not (x <> y) or false then
-  begin
-    writeln('sao iguais')
-  end
-  else
-  begin
-    writeln('sao diferentes')
-  end;
-  if (x <= y) then
-  begin
-    mostrar(x, dobro(y))
-  end;
-  if true then
-  begin
-    write(total)
-  end
-end.
+Program Fibonacci;
+Var termo1, termo2, aux, cont, quantos : Integer;
+Begin
+    writeln('----------FIBONACCI------------');
+    termo1 := 1;
+    termo2 := 1;
+    quantos := 0;
+    aux := 0;
+    write('Informe a quantidade de numeros que deseja ver da sequencia fibonacci: ');
+    read(quantos);
+    writeln(termo1);
+    writeln(termo2);
+    cont := 2;
+    while (cont < quantos) do
+    begin
+        aux := termo1 + termo2;
+        writeln(aux);    
+        termo1 := termo2;
+        termo2 := aux;
+        cont := cont + 1; 
+    end; 
+End.
